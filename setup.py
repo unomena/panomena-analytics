8dc682c1f37cc8f92a84a47dcc7f8da3978a99fa
Jurie-Jan Botha <juriejanbotha@gmail.com>
2011-09-02 12:10:43 +0200

    Bumped version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='panomena-analytics',
     description='Panomena Analytics',
-    version='0.0.1',
+    version='0.0.2',
     author='',
     license='Proprietory',
     url='http://www.unomena.com/',
